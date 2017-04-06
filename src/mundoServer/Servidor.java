0fe6be0c2d3dd683722e399b5611f5f752c701eb
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-04-06 00:24:05 -0500

    ya lee y guarda mensajes de un cliente en un file

--- src/mundoServer/Servidor.java
+++ src/mundoServer/Servidor.java
@@ -1,8 +1,20 @@
 package mundoServer;
 
 
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.*;
+import java.util.Date;
+
+import mundoClient.Objeto;
 
 public class Servidor {
 
@@ -14,7 +26,7 @@ public class Servidor {
     public void run(int port) {    
       try {
         DatagramSocket serverSocket = new DatagramSocket(port);
-        byte[] receiveData = new byte[1024];
+        byte[] receiveData = new byte[512];
 
         System.out.printf("Listening on udp:%s:%d%n",
                 InetAddress.getLocalHost().getHostAddress(), port);     
@@ -26,12 +38,30 @@ public class Servidor {
               serverSocket.receive(receivePacket);
               String sentence = new String( receivePacket.getData(), 0,
                                  receivePacket.getLength() );
-              System.out.println("RECEIVED: " + sentence);
+              Objeto objectoEntrante= (Objeto)deserialize(receivePacket.getData());
+              //llega esto -> 192.168.56.1:63365
+              String complete = receivePacket.getSocketAddress() +"";
+              String cliente = complete.split(":")[0];
+              System.out.println(cliente);
+              System.out.println("RECEIVED: " +objectoEntrante.getPos()+"-"+ objectoEntrante.getTimestamp());
               
+              File log = new File("./data/"+cliente+".txt");
+              PrintWriter out = new PrintWriter(new FileWriter(log, true));
+              
+              out.write(objectoEntrante.getPos()+": "+(System.currentTimeMillis() -objectoEntrante.getTimestamp().getTime())+ "\n  ");
+              out.flush();
+              out.close();
+        
         }
-      } catch (IOException e) {
+      } catch (Exception e) {
               System.out.println(e);
       }
       // should close serverSocket in finally block
+    }
+    
+    private Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream b = new ByteArrayInputStream(bytes);
+        ObjectInputStream o = new ObjectInputStream(b);
+        return (Objeto)o.readObject();
     }
 }
