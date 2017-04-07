e2f2078415cc2be5dbb61de3cc8cb38ce1bd7ade
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-04-07 18:51:15 -0500

    escribe en el file y maneja el numero de objetos faltantes y los que ya llegaron

--- src/mundoServer/ManejadorConteoMensajes.java
+++ src/mundoServer/ManejadorConteoMensajes.java
@@ -1,5 +1,8 @@
 package mundoServer;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import mundoClient.Objeto;
@@ -38,28 +41,36 @@ public class ManejadorConteoMensajes extends Thread {
 		
 		int i =clientes.lastIndexOf(cliente);
 		if(i==-1){
+			//nuevo cliente
+			
 			clientes.add(cliente);
 			
 			i= clientes.size()-1;//la pos de este cliente
 			System.out.println("crea cliente");
 			
-			//los mensajes acumulados hasta ahora son: 0
-			cuantosMensajesPorCliente.add(i,new Long(0));
+			//faltan por mandar objeto.getPos()
+			cuantosMensajesPorCliente.add(i,-objeto.darTotal());
 		}
 		
-		//por cada mensaje estoy añandiendo en la lista la suma de las pos.
-		//al llegar n mensajes, la lista del cliente va a contener el numero n(n+1)/2
-		//pero cuando llegue el ultimo objeto de terminación, su pos será -((n*n+1)/2)
+		//por cada mensaje estoy añandiendo en la lista de mensajes +1.
+		//al llegar n mensajes, la lista del cliente va a contener el numero n
+		//pero cuando llegue el ultimo objeto de terminación, su pos será -n
 		//osea que cuantos mensajes por cliente debe ser 0 cuando todos los mensajes sean enviados
 		//mientras no sea 0, o se perdieron paquetes del cliente o no ha terminado de mandar
-		cuantosMensajesPorCliente.add(i, (cuantosMensajesPorCliente.get(i) + objeto.getPos())  );
+		cuantosMensajesPorCliente.add(i, (cuantosMensajesPorCliente.get(i) + 1)  );
 		
-		if(cuantosMensajesPorCliente.get(i) != 0){
-			System.out.println("Se han perdido paquetes o no ha terminado. La cuenta es: "+cuantosMensajesPorCliente.get(i));
-		}
-		else{
+		System.out.println("\n El cliente "+ cliente +" paquetes que faltan "+(-cuantosMensajesPorCliente.get(i)));
+		//los que llegaron son el total - los que faltan
+		System.out.println("\n El cliente "+ cliente +" paquetes que llegaron "+(objeto.darTotal()+cuantosMensajesPorCliente.get(i)));
+		
+		if(cuantosMensajesPorCliente.get(i) == 0){
 			System.out.println("\n El cliente "+ cliente +" NO PERDIO ningun paquete");
 		}
+		
+		try {
+			//perdidas son los que faltaron
+			escribirArchivo(msg, -cuantosMensajesPorCliente.get(i)+"" , "tiempo");
+		} catch (Exception e) {e.printStackTrace();}
 	}
 	
 	public void procesarMensaje(String cliente, Objeto objetoEntrante){
@@ -69,6 +80,25 @@ public class ManejadorConteoMensajes extends Thread {
         synchronized (mensajes){
         	mensajes.add(new Mensaje(cliente,objetoEntrante));        	
         }
+	}
+	
+	public void escribirArchivo(Mensaje msg, String perdidas, String tiempoPromedio) throws Exception{
+		String cliente = msg.clie;
+		Objeto objetoEntrante = msg.obje;
+		
+		//escribe en el file el numero del mensaje y el tiempo
+        File log = new File("./data/"+cliente+".txt");
+        PrintWriter out = new PrintWriter(new FileWriter(log, true));
+        
+        out.write(objetoEntrante.getPos()+": "+(System.currentTimeMillis() -objetoEntrante.getTimestamp().getTime())+ "\n  ");
+        
+        if(msg.obje.darTotal() == msg.obje.getPos()){
+        	//es el ultimo mensaje
+        	out.write(" Perdidas/faltas totales: "+ perdidas+ " Objetos recibidos: "+msg.obje.darTotal() + " Tiempo promedio: "+ tiempoPromedio+ " ms");
+        }
+        
+        out.flush();
+        out.close();
 	}
 	
 	public class Mensaje{
