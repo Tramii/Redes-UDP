852942c03e2505a6dc515676649659d7ef6bbf67
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-04-09 23:25:53 -0500

    pruebas

--- src/ServidorArchivos/ServidorArchivos.java
+++ src/ServidorArchivos/ServidorArchivos.java
@@ -24,10 +24,31 @@ public class ServidorArchivos {
         server.run(port);
     }
     
-    
-    
     public ServidorArchivos(){
-
+    	//lee el archivo
+    	
+    	file = new File("./descargas/"+tituloAPedir);
+        // Get the size of the file
+        
+        fos = new FileOutputStream(file);
+        
+        
+        byte [] bytes  = new byte [1024*16];
+        String tamanoEnMB = inFromServerLine.readLine();
+        System.out.println("\n ya va a recibir el archivo que pesa "+tamanoEnMB);
+        //int bytesRead = 0;
+        int current = 0;
+        int i=0;
+        int count;
+        while ((count = inFromServer.read(bytes)) > 0) {
+            fos.write(bytes, 0, count);
+            i++;
+            current+=count;
+            System.out.println("Llego paquete. Escribiendo en el archivo el mensaje numero "+i);
+            System.out.println("Descargado hasta el momento: "+current + "");
+            //descomentar la linea de abajo para ver el contenido del paquete
+            //System.out.println("contenido del paquete "+ " "+new String(bytes));
+        }
     }
 
     public void run(int port) {    
